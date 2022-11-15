44776b8c3628c3649f454a4236f6e3fc8b8c7382
Newton Ombese <newtonombese1@gmail.com>
2022-11-15 09:47:25 +0300

    adding bootstrap

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,11 +4,12 @@ function Home() {
   return (
     <div className='container'>
         <div className='row'>
-          <div className='col-md-6'>
-            
+          <div className='col-md-6 d-flex align-items-center justify-content-center'>
+            <h2>We care about your helath</h2>
+
           </div>
-          <div className='col-md-6'>
-            
+          <div className='col-md-6 d-flex align-items-center justify-content-center'>
+            <img style={{height: '80vh'}}src='https://images.pexels.com/photos/6749773/pexels-photo-6749773.jpeg?auto=compress&cs=tinysrgb&w=600' alt='doctor and nurse' />
           </div>
         </div>
     </div>
