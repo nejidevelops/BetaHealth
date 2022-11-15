626ec2a2ba20a485d8af006a4589402548dee2bb
Newton Ombese <newtonombese1@gmail.com>
2022-11-15 09:14:58 +0300

    getting my navbar ready for edits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import NavBar from './components/Home/NavBar';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <NavBar />
+    </>
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container'>
+        Home
+    </div>
+  )
+}
+
+export default Home
